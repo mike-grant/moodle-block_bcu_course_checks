57a8d1ea0a519e5517c8ef0a88dab9dab16cbd52
unknown <id119638@.staff.uce.ac.uk>
2014-05-14 14:43:50 +0100

    Initial Commit

---
+++ lang/en/block_yourcourse.php
@@ -0,0 +1,17 @@
+<?php
+/**
+Language file for the tutorial block, defining strings to be used in the block. 
+From tutorial at http://docs.moodle.org/dev/Blocks
+Called from the init() in the main block file /moodle/blocks/yourcourse/block_yourcourse.php
+*/
+
+$string['pluginname'] = 'Your Course block';
+$string['yourcourse'] = 'Your Course';
+$string['yourcourse:addinstance'] = 'Add a new Your Course block';
+$string['yourcourse:myaddinstance'] = 'Add a new Your Course block to the My Moodle page';
+
+
+
+
+
+?>
---
+++ version.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+Version information for the Your Course plugin.
+For reference, see blocks tutorial at 
+http://docs.moodle.org/dev/Blocks
+*/
+# NB: $plugin is a Moodle-defined variable, and must be used
+# Version of the plugin itself - I've inserted the datetime of writing this file
+# ie 3pm May 9th 2014
+$plugin->version = 2014050915;  // YYYYMMDDHH (year, month, day, 24-hr time)
+# Minimum Moodle version required
+$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+
+
+
+
+
+?>
