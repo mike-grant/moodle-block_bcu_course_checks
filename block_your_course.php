90d2c34f744b5fcd2caa0d98cf5efc04db6f6b3f
unknown <id109140@.staff.uce.ac.uk>
2014-06-02 13:32:15 +0100

    Renamed file to conform with Moodle naming convention, added code to build your course feed url from current faculty / course id.

--- block_your_course.php
+++ block_your_course.php
@@ -6,65 +6,57 @@ Main file for the Your Course block. As reference, see blocks tutorial at:
 http://docs.moodle.org/dev/Blocks
 
 */
-class block_yourcourse extends block_base
+class block_your_course extends block_base
 {
 	public function init()
 	{
-		# Get title defined in language file lang/en/block_yourcourse.php 
-		$this -> title = get_string('yourcourse', 'block_yourcourse');
-	
-	
+		# Get title defined in language file lang/en/block_your_course.php 
+		$this -> title = get_string('yourcourse', 'block_your_course');	
 	}
 
 	public function get_content() 
 	{
-		# Matt's test url
-		$url = 'http://www.fredriley.org.uk/test/48.xml';
-		# Let's try to get XML data from the URL using the Moodle func download_file_content() defined in lib/filelib.php
+		global $CFG, $COURSE;
+			
+		$url = 'http://icitydelta.bcu.ac.uk/api/yourcourse/' . $CFG->facshort . '/' . $COURSE->id;
+			
+		$headers = array(
+            'Content-Type' => 'application/xml',
+            'Accept' => 'application/xml'
+        );		
+		
 		$data = download_file_content($url, null, null, false, '300', '20', true, null, false);
+		
 		if (!$data)
 		{
-			$content = "Unable to retrieve your course data: bummer";
+			$content = "Unable to Retrieve your course data: bummer";
 		}
 		else 
-		{
-			# Let's parse this XML fecker!
+		{			
 			//$content = "Your course data retrieved ok";
 			# PHP manual: "simplexml_load_string — Interprets a string of XML into an object "
 			$module = simplexml_load_string($data);
-			# Use object notation to get module details
+			# Use object notation to get module details			
 			$leader = $module -> ModuleLeader[0] -> DisplayName;
 			$email = $module -> ModuleLeader -> Email;
 			$tel = $module -> ModuleLeader -> Phone;
 			$module_details_url = $module -> YourCourseModuleUrl;
-			$module_guide_url = $module -> ModuleGuideUrl;
+			$module_guide_url = $module -> ModuleGuideUrl;			
 			$content = "<h3>Module information</h3>" . "<p>Leader: <a href=\"mailto:$email\">$leader</a><br>" . "Tel: $tel<br>" . 
 				"<a href=\"$module_details_url\" target=\"modulewin\">Module details</a><br>" . 
-				"<a href=\"$module_guide_url\" target=\"modulewin\">Module guide</a></p>"; 
-			
+				"<a href=\"$module_guide_url\" target=\"modulewin\">Module guide</a></p>";		  				
 		}
+		
 		if ($this->content !== null) 
 		{
-		  return $this->content;
+		  // return $this->content;
 		}
 	 
-		$this->content         =  new stdClass;
+		$this->content = new stdClass;
 		$this -> content -> text = $content;
 		//$this->content->text   = 'Your Course: some content';
 		$this->content->footer = 'Your Course: footer';
 	 
 		return $this->content;
 	}
-	
-
-
 } // end class
-
-
-
-
-
-
-
-
-?>
