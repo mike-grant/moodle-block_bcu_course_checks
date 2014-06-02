d9637cf39808bf107af0933004ca456a9502a254
unknown <id109140@.staff.uce.ac.uk>
2014-06-02 14:23:05 +0100

    Added $headers to download_file_contents feed call to get back XML rather than json, changed lang file name so that plugin name appears correctly and bumped version to force test system to update.

--- block_your_course.php
+++ block_your_course.php
@@ -9,8 +9,7 @@ http://docs.moodle.org/dev/Blocks
 class block_your_course extends block_base
 {
 	public function init()
-	{
-		# Get title defined in language file lang/en/block_your_course.php 
+	{		 
 		$this -> title = get_string('yourcourse', 'block_your_course');	
 	}
 
@@ -25,38 +24,36 @@ class block_your_course extends block_base
             'Accept' => 'application/xml'
         );		
 		
-		$data = download_file_content($url, null, null, false, '300', '20', true, null, false);
+		$data = download_file_content($url, $headers, null, false, '300', '20', true, null, false);
 		
 		if (!$data)
 		{
-			$content = "Unable to Retrieve your course data: bummer";
+			// replace with empty string after testing whcih will prevent block from shwoing at all
+			$content = "It does not look like there is any YourCourse information for this module"; 
 		}
 		else 
-		{			
-			//$content = "Your course data retrieved ok";
-			# PHP manual: "simplexml_load_string — Interprets a string of XML into an object "
-			$module = simplexml_load_string($data);
-			# Use object notation to get module details			
+		{					
+			$module = simplexml_load_string($data);			
+			
 			$leader = $module -> ModuleLeader[0] -> DisplayName;
-			$email = $module -> ModuleLeader -> Email;
+			$email = $module->ModuleLeader->Email;
 			$tel = $module -> ModuleLeader -> Phone;
 			$module_details_url = $module -> YourCourseModuleUrl;
 			$module_guide_url = $module -> ModuleGuideUrl;			
 			$content = "<h3>Module information</h3>" . "<p>Leader: <a href=\"mailto:$email\">$leader</a><br>" . "Tel: $tel<br>" . 
 				"<a href=\"$module_details_url\" target=\"modulewin\">Module details</a><br>" . 
-				"<a href=\"$module_guide_url\" target=\"modulewin\">Module guide</a></p>";		  				
+				"<a href=\"$module_guide_url\" target=\"modulewin\">Module guide</a></p>";			
 		}
 		
 		if ($this->content !== null) 
 		{
-		  // return $this->content;
+		  return $this->content;
 		}
 	 
 		$this->content = new stdClass;
-		$this -> content -> text = $content;
-		//$this->content->text   = 'Your Course: some content';
+		$this -> content -> text = $content;		
 		$this->content->footer = 'Your Course: footer';
 	 
 		return $this->content;
-	}
+    }
 } // end class
